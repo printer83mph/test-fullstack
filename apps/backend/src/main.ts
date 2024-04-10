02b5f76e2dee5a040ca636856b0be7125af3c84d
Thomas Shaw <printer.83mph@gmail.com>
2024-04-10 18:07:42 -0400

    Add socket stuff!!

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,12 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
 
 // read environment variables from .env file
 dotenv.config();
 const PORT = process.env.PORT ?? 8000;
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [process.env.FRONTEND_URL ?? 'http://localhost:3000'],
+    methods: ['GET', 'POST'],
+  },
+});
+
 app.use(express.json());
 
 app.use(
@@ -35,8 +45,20 @@ app.post('/api/stuff', (req, res) => {
   return res.send('success!');
 });
 
+io.on('connection', (socket) => {
+  socket.data.name = 'John';
+
+  socket.on('message', (message: string) => {
+    io.emit('message', socket.data.name, message);
+  });
+});
+
+setInterval(() => {
+  io.emit('ping');
+}, 1000);
+
 // listen
-app.listen(PORT as number, '0.0.0.0', () => {
+server.listen(PORT as number, () => {
   // eslint-disable-next-line no-console
   console.log(`Now listening on port ${PORT}.`);
 });
--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -1,8 +1,35 @@
+import { useEffect, useRef, useState } from 'react';
 import './app.css';
 import useStuff from './hooks/use-stuff';
+import { Socket, io } from 'socket.io-client';
 
 function App() {
   const { stuff, addNewThing, error, isLoading } = useStuff();
+  const [messages, setMessages] = useState<{ user: string; message: string }[]>(
+    [],
+  );
+
+  const [message, setMessage] = useState('');
+  const socketRef = useRef<Socket>(null!);
+
+  useEffect(() => {
+    const socket = io(import.meta.env.VITE_BACKEND_URL);
+
+    socket.on('ping', () => {
+      console.log('pong!');
+    });
+
+    socket.on('message', (id: string, message: string) => {
+      setMessages((m) => [...m, { user: id, message }]);
+    });
+
+    socketRef.current = socket;
+
+    // cleanup function: disconnect socket
+    return () => {
+      socket.disconnect();
+    };
+  }, [setMessages]);
 
   return (
     <>
@@ -14,6 +41,28 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <input
+          type="text"
+          onChange={(evt) => setMessage(evt.target.value)}
+          value={message}
+        />
+        <button
+          type="button"
+          onClick={() => {
+            socketRef.current.emit('message', message);
+          }}
+        >
+          Send!
+        </button>
+        <ul>
+          {messages.map(({ user, message }) => (
+            <li key={`${user}-${message}`}>
+              {user}: {message}
+            </li>
+          ))}
+        </ul>
+      </div>
       <ul>
         {error && <div>Something went wrong</div>}
         {isLoading && <div>Loading...</div>}
